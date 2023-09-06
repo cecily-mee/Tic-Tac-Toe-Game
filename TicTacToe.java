43b33c63ce8decd2cc71af453e37c05dc48e18d9
cecily-mee <143897317+cecily-mee@users.noreply.github.com>
2023-09-06 23:31:26 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+        char currentPlayer = 'X';
+
+        boolean gameOver = false;
+        int moves = 0;
+
+        while (!gameOver) {
+            printBoard(board);
+            int[] move = getPlayerMove(board, currentPlayer);
+            int row = move[0];
+            int col = move[1];
+
+            if (isValidMove(board, row, col)) {
+                board[row][col] = currentPlayer;
+                moves++;
+
+                if (checkWin(board, currentPlayer)) {
+                    printBoard(board);
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    gameOver = true;
+                } else if (moves == 9) {
+                    printBoard(board);
+                    System.out.println("It's a draw!");
+                    gameOver = true;
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+
+    public static int[] getPlayerMove(char[][] board, char currentPlayer) {
+        Scanner scanner = new Scanner(System.in);
+        int[] move = new int[2];
+
+        System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
+        move[0] = scanner.nextInt();
+        move[1] = scanner.nextInt();
+
+        return move;
+    }
+
+    public static boolean isValidMove(char[][] board, int row, int col) {
+        return (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ');
+    }
+
+    public static boolean checkWin(char[][] board, char player) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true;
+            }
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                return true;
+            }
+        }
+
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true;
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true;
+        }
+
+        return false;
+    }
+}
